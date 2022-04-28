211ccc38108c4f7cad4f1c014fbdf44f362b632f
Mike M <mikester70387@gmail.com>
2022-04-28 11:20:41 -0400

    Finished hw9 & lab8

--- hw9/life.c
+++ hw9/life.c
@@ -1,4 +1,5 @@
 #define _POSIX_C_SOURCE 200809L
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
@@ -6,45 +7,48 @@
 #include <string.h>
 #include <sys/time.h>
 
-#define DEFAULT_FILE_NAME "start0.txt"
-#define DEFAULT_CYCLES 100
-#define DEFAULT_PRINT_FLAG 1
-#define DEFAULT_NUM_THREADS 2
+#define DEFAULT_FILE_NAME    "start0.txt"
+#define DEFAULT_CYCLES       100
+#define DEFAULT_PRINT_FLAG   1
+#define DEFAULT_NUM_THREADS  2
 
-#define REFRESH_MILLISEC 1000 / 30
+#define REFRESH_MILLISEC 1000/30
+#define cycle(x) x % 2
+
+pthread_barrier_t barrier;
 
 void msleep(int tms) {
     struct timeval tv;
-    tv.tv_sec = tms / 1000;
+    tv.tv_sec  = tms / 1000;
     tv.tv_usec = (tms % 1000) * 1000;
-    select(0, NULL, NULL, NULL, &tv);
+    select (0, NULL, NULL, NULL, &tv);
 }
 
 typedef struct {
     int row;
     int col;
-    char **src;
+    char** src;
 } Board;
 
-typedef struct {
+typedef struct thread_result {
+    Board* a;
     Board* b;
-    int g;
     int cycles;
-    int printFlag;
+    int start;
+    int end;
 } thread_result;
 
-Board *makeBoard(int r, int c) {
-    Board *p = malloc(sizeof(Board));
+Board* makeBoard(int r, int c) {
+    Board* p = malloc(sizeof(Board));
     p->row = r;
     p->col = c;
-    p->src = malloc(sizeof(char *) * r);
-    for (int i = 0; i < r; i++)
-        p->src[i] = malloc(sizeof(char) * c);
-
+    p->src = malloc(sizeof(char*)*r);
+    for(int i=0; i<r; i++)
+        p->src[i] = malloc(sizeof(char)*c);
     return p;
 }
 
-void freeBoard(Board *b) {
+void freeBoard(Board* b) {
     for (int i = 0; i < b->row; i++)
         free(b->src[i]);
 
@@ -52,11 +56,11 @@ void freeBoard(Board *b) {
     free(b);
 }
 
-Board *readBoard(char *fName) {
+Board* readBoard(char* fName) {
     int row, col;
-    FILE *src = fopen(fName, "r");
+    FILE* src = fopen(fName,"r");
     fscanf(src, "%d %d\n", &row, &col);
-    Board *rv = makeBoard(row, col);
+    Board* rv = makeBoard(row, col);
 
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
@@ -65,81 +69,77 @@ Board *readBoard(char *fName) {
         }
 
         char skip = fgetc(src);
-        while (skip != '\n')
-            skip = fgetc(src);
+        while (skip != '\n') skip = fgetc(src);
     }
 
     fclose(src);
     return rv;
 }
 
-void saveBoard(Board *b, FILE *fd) {
+void saveBoard(Board* b, FILE* fd) {
     for (int i = 0; i < b->row; i++) {
-        fprintf(fd, "|");
+        fprintf(fd,"|");
         for (int j = 0; j < b->col; j++)
-            fprintf(fd, "%c", b->src[i][j] ? '*' : ' ');
-
-        fprintf(fd, "|\n");
+            fprintf(fd,"%c",b->src[i][j] ? '*' : ' ');
+        fprintf(fd,"|\n");
     }
 }
 
-void printBoard(Board *b) {
-    printf("\033\143"); /* this is to clear the terminal */
+void printBoard(Board* b) {
+    printf("\033\143");  
     saveBoard(b, stdout);
 }
 
-int liveNeighbors(int i, int j, Board *b) {
-    const int pc = (j - 1) < 0 ? b->col - 1 : j - 1;
+int liveNeighbors(int i, int j, Board* b) {
+    const int pc = (j-1) < 0 ? b->col-1 : j - 1;
     const int nc = (j + 1) % b->col;
-    const int pr = (i - 1) < 0 ? b->row - 1 : i - 1;
+    const int pr = (i-1) < 0 ? b->row-1 : i - 1;
     const int nr = (i + 1) % b->row;
     
-    int xd[8] = {pc, j, nc, pc, nc, pc, j, nc};
-    int yd[8] = {pr, pr, pr, i, i, nr, nr, nr};
+    int xd[8] = {pc , j , nc, pc, nc, pc , j , nc };
+    int yd[8] = {pr , pr, pr, i , i , nr , nr ,nr };
     int ttl = 0;
-    
     for (int k = 0; k < 8; k++)
         ttl += b->src[yd[k]][xd[k]];
 
     return ttl;
 }
 
-void evolveBoard(Board *in, Board *out) {
+void evolveBoard(Board* in, Board* out, int r1, int r2) {
     static int rule[2][9] = {
-        {0, 0, 0, 1, 0, 0, 1, 0, 0},
-        {0, 0, 1, 1, 0, 0, 0, 0, 0}
+        { 0, 0, 0, 1, 0, 0, 1, 0, 0 },
+        { 0, 0, 1, 1, 0, 0, 0, 0, 0 }
     };
 
-    for (int i = 0; i < in->row; i++) {
+    for (int i = r1; i < r2; i++) {
         for (int j = 0; j < in->col; j++) {
-            int ln = liveNeighbors(i, j, in);
-            int c = in->src[i][j];
-            out->src[i][j] = rule[c][ln];
+            int lin = liveNeighbors(i, j, in);
+            int con  = in->src[i][j];
+            out->src[i][j] = rule[con][lin];
         }
     }
 }
 
-void* evolve(void* arg) {
-    thread_result* res = (thread_result*) arg;
-    int g = res->g;
-    int cycles = res->cycles;
-    int printFlag = res->printFlag;
-    Board* b[2] = res->b;
+void* evolveThread(void* arg) {
+    thread_result* data = (thread_result*)arg;
+    Board* b1 = data->a;
+    Board* b2 = data->b;
+    int start = data->start;
+    int end = data->end;
+    int cycles = data->cycles;
 
-    for (int g = g; g < cycles; g++) {
-        if (printFlag) {
-            printBoard((Board*) b[g % 2]);
-            msleep(REFRESH_MILLISEC);
-        }
+    Board* boards[2] = { b1, b2 };
 
-        evolveBoard(b[g % 2], b[1 - g % 2]);
+    for (int g = 0; g < cycles; g++) {
+        evolveBoard(boards[cycle(g)], boards[1 - cycle(g)], start, end);
+        pthread_barrier_wait(&barrier);
     }
 
     pthread_exit(NULL);
 }
 
-int main(int argc, char *argv[]) {
-    char *file_name = (argc < 2 ? DEFAULT_FILE_NAME : argv[1]);
+int main(int argc, char* argv[]) {
+    char* file_name = (argc < 2 ? DEFAULT_FILE_NAME : argv[1]);
     int cycles = (argc < 3 ? DEFAULT_CYCLES : atoi(argv[2]));
     int printFlag = (argc < 4 ? DEFAULT_PRINT_FLAG : atoi(argv[3]));
     int num_threads = (argc < 5 ? DEFAULT_NUM_THREADS : atoi(argv[4]));
@@ -149,42 +149,50 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    Board *board1 = readBoard(file_name);
-    Board *board2 = makeBoard(board1->row, board1->col);
-    Board *b[2] = {board1, board2};
-
-    pthread_t threads[num_threads];
-    thread_result results[num_threads];
-
-    for (int i = 0; i < num_threads; i++) {
-        results[i].b = b;
-        results[i].g = i;
-        results[i].cycles = cycles;
-        results[i].printFlag = printFlag;
-        pthread_create(&threads[i], NULL, evolve, &results[i]);
-    }
+    thread_result result[num_threads];  
+    pthread_t threads[num_threads];  
     
+    Board* a = readBoard(file_name);
+    Board* b = makeBoard(a->row, a->col);
+    Board* boards[2] = {a, b};
+    
+    pthread_barrier_init(&barrier, NULL, num_threads);
+        
+    for (int i = 0; i < num_threads; i++) {
+        int start = (i * a->row) / num_threads;                                 
+        int end = i == num_threads - 1
+            ? a->row
+            : ((i + 1) * a->row) / num_threads;
+        
+        thread_result data;
+        data.a = a;
+        data.b = b;
+        data.cycles = cycles;
+        data.start = start;
+        data.end = end;
+
+        result[i] = data;
+        pthread_create(&threads[i], NULL, evolveThread, (void *)&(result[i]));
+    }
+
     for (int i = 0; i < num_threads; i++)
         pthread_join(threads[i], NULL);
 
-    // combine boards from all threads
-    for (int i = 0; i < board1->row; i++) {
-        for (int j = 0; j < board1->col; j++) {
-            int ttl = 0;
-            for (int k = 0; k < num_threads; k++)
-                ttl += b[k % 2]->src[i][j];
-
-            board1->src[i][j] = ttl > 0;
+    for (int k = 0; k < cycles; k++) {
+        if (printFlag) {
+            printBoard(boards[k%2]);
+            msleep(REFRESH_MILLISEC);
         }
     }
 
-    if (printFlag) printBoard(b[1 - cycles % 2]);
+    if (printFlag) printBoard(boards[1 - cycle(cycles)]);
 
-    FILE *final = fopen("final_threads.txt", "w");
-    saveBoard(b[1 - cycles % 2], final);
+    FILE* final = fopen("final_threads.txt","w");
+    saveBoard(boards[1 - cycle(cycles)], final);
     fclose(final);
-
-    freeBoard(board1);
-    freeBoard(board2);
+    
+    pthread_barrier_destroy(&barrier);
+    freeBoard(a);
+    freeBoard(b);
     return 0;
 }
